e0f6c1e4b3a01b9c36d6a76b9198c8933af828c0
dokibacsi <kvalcsi02@gmail.com>
2022-03-29 16:17:09 +0200

    8. feladat

--- kecske.js
+++ kecske.js
@@ -16,10 +16,41 @@ function init(){
     /*Készítő nevének megváltoztatása*/
     $('footer p')[0].innerHTML = "Katona Valentin";
     $('footer p')[0].style.textAlign = "center";
-    $('footer.p')[0].style.fontSize = "20px";
+    $('footer p')[0].style.fontSize = "20px";
     
-    var tomb = document.querySelectorAll('#bal img');
+    var kepTomb = document.querySelectorAll('#bal img');
     for(let index = 0; index < kepTomb.length; index++){
         kepTomb[index].addEventListener("click", kepEleres)
+        kepTomb[index].addEventListener("mousemove", Kiemeles)
+        kepTomb[index].addEventListener("mouseover", Leemeles)
     }
+}
+var tomb = [];
+function kepEleres(event){
+    tomb.push(event.target.alt); // event target = eseményt kiváltó
+    console.log(tomb);
+
+    if(tomb.indexOf("kecske") >= 0 && tomb.indexOf("farkas") >= 0){
+        alert("Nem jó választás!")
+        tomb.pop();
+    }
+    else if(tomb.indexOf("kecske") >= 0 && tomb.indexOf("kaposzta") >= 0){
+        alert("Nem jó választás!")
+        tomb.pop();
+    }
+    else{
+    event.target.style.display = "none";
+    var kep = '<img src=`${event.target.src}`alt=`${event.target.alt}`/>';
+    console.log(kep);
+    ID("csonak").innerHTML =+ kep;
+}
+}
+
+function Kiemeles(event){
+    console.log("kiemel")
+    event.target.ClassName = "kiemel";
+}
+
+function Leemeles(event){
+    event.target.ClassName = "";
 }
