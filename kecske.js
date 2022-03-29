872a1f3dffed30f1e2e491e211c3286e1bb141b4
dokibacsi <kvalcsi02@gmail.com>
2022-03-29 14:57:03 +0200

    2. feladat

--- kecske.js
+++ kecske.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", init);
+window.addEventListener("load", betolt);
 
 function ID(elem){
     return document.getElementById(elem);
@@ -8,8 +8,13 @@ function CLS(elem){
     return document.getElementsByClassName(elem);
 }
 
-function init(){
-    
+function $(elem){
+    return document.querySelectorAll(elem);
+}
 
-
+function betolt(){
+    /*Készítő nevének megváltoztatása*/
+    $('footer p')[0].innerHTML = "Katona Valentin";
+    $('footer p')[0].style.textAlign = "center";
+    $('footer.p')[0].style.fontSize = "20px";
 }
