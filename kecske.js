1eb0bcb6b020376c2356c637fb54fa268ef9de96
dokibacsi <kvalcsi02@gmail.com>
2022-03-29 14:44:09 +0200

    css kialakitas

---
+++ kecske.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", init);
+
+function ID(elem){
+    return document.getElementById(elem);
+}
+
+function CLS(elem){
+    return document.getElementsByClassName(elem);
+}
+
+function init(){
+    
+
+
+}
